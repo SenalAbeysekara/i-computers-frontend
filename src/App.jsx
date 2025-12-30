51c56524a68f04e9dee4882b4079673207805255
SenalAbeysekara <senaldushyantha@gmail.com>
2025-12-30 22:32:05 +0530

    Day 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import OnSaleNow from "./components/onSaleNow.jsx";
-import UserData from "./components/userData.jsx";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home.jsx";
+import AdminPage from "./pages/admin.jsx";
+import LoginPage from "./pages/login.jsx";
 
 function App() {
-	return (
-		<div className="bg-red-500">
-			<UserData />
-			<OnSaleNow />
-		</div>
-	);
-}
+    return (
+        <div className="w-full h-screen">
+			<Routes>
+                <Route path="/" element={<HomePage/>} />
+                 {/* all-* koma awath admin ekt enawa */}
+                <Route path="/admin/*" element={< AdminPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </div>
+    );
+}    
 
-export default App
+export default App;
